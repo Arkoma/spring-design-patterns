62364e997da39a4f9503251ba4f25fbbeab055ed
Aaron Burk <aburk@veracode.com>
2021-08-06 17:09:50 -0400

    implement factory pattern

--- src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
+++ src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
@@ -2,6 +2,7 @@ package com.frankmoley.lil.designpatternsapp.controller;
 
 
 import com.frankmoley.lil.designpatternsapp.factory.Pet;
+import com.frankmoley.lil.designpatternsapp.factory.PetFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class AppController {
 
+    private PetFactory petFactory;
+
+    @Autowired
+    public AppController(PetFactory petFactory) {
+        this.petFactory = petFactory;
+    }
+
     @GetMapping
     public String getDefault(){
         return "{\"message\": \"Hello World\"}";
     }
 
+    @PostMapping("adopt/{type}/{name}")
+    public Pet adoptPet(@PathVariable String type, @PathVariable String name) {
+        final Pet pet = this.petFactory.adoptPet(type);
+        pet.setName(name);
+        pet.feed();
+        return pet;
+    }
 }
